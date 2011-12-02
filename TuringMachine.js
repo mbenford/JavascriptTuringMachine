6588efc250e99224c6a782655a0ba8491ffacd1b
Michael Benford <michael@michaelbenford.net>
2011-12-02 02:44:26 -0200

    Computacao implementada

--- TuringMachine.js
+++ TuringMachine.js
@@ -3,57 +3,76 @@ function TuringMachine() {
 
 TuringMachine.prototype = {
 	setStates: function(states) {
-		this._states = states.split("");
+		this._states = this._removeSpaces(states).split("");
 	},
 	
 	setInputSymbols: function(symbols) {
-		this._inputSymbols = symbols.split("");
+		this._inputSymbols = this._removeSpaces(symbols).split("");
 	},
 	
 	setInitialState: function(state) {
-		this._initialState = state;
+		this._initialState = this._removeSpaces(state);
 	},
 	
 	setFinalStates: function(states) {
-		this._finalStates = states.split("");
+		this._finalStates = this._removeSpaces(states).split("");
 	},
 	
-	setTransitionTable: function(table) {
+	setTransitions: function(transitions) {
+		var table = new TransitionTable();
+		
+		for (t in transitions) {
+			var elements = this._removeSpaces(transitions[t]).split(",");
+			var state = elements[0];
+			var symbol = elements[1];
+			var nextState = elements[2];
+			var nextSymbol = elements[3];
+			
+			table.add(state, symbol, nextState, nextSymbol);
+		}
+		
 		this._table = table;
 	},
 	
-	run: function(input) {		
-		this._tape = input.split("");
+	setTape: function(tape) {
+		this._tape = tape.split("");
+	},
+	
+	getTape: function() {
+		return this._tape.toString().replace(/,/g, "");
+	},
+	
+	_removeSpaces: function(s) {
+		return s.replace(/ /g, "");
+	},
+	
+	run: function() {
 		this._currentState = this._initialState;
 		this._currentPosition = 0;
 		
 		while (!this._isInFinalState()) {
 			this._currentSymbol = this._getCurrentSymbol();
 			
-			var nextState = this._getNextState();
-			var nextSymbol = this._getNextSymbol();
+			var nextStateAndChar = this._getNextStateAndChar();
+			if (nextStateAndChar == null) break;
 			
-			if (nextSymbol == "<=") this._moveLeft();
-			else if (nextSymbol == "=>") this._moveRight();
+			var nextState = nextStateAndChar[0];
+			var nextSymbol = nextStateAndChar[1];
+			
+			if (nextSymbol == "<<") this._moveLeft();
+			else if (nextSymbol == ">>") this._moveRight();
 			else this._write(nextSymbol);
-			
-			this._currentSymbol = nextSymbol;
+						
 			this._currentState = nextState;
 		}
-		
-		return this._tape;
 	},
 	
 	_getCurrentSymbol: function() {
 		return this._tape[this._currentPosition];
 	},
 	
-	_getNextState: function() {
-		return this._table.get(this._currentState, this._currentSymbol).split(",")[0];
-	},
-	
-	_getNextSymbol: function() {
-		return this._table.get(this._currentState, this._currentSymbol).split(",")[1];
+	_getNextStateAndChar: function() {		
+		return this._table.get(this._currentState, this._currentSymbol);
 	},
 	
 	_moveLeft: function() {
@@ -63,10 +82,10 @@ TuringMachine.prototype = {
 	
 	_moveRight: function() {
 		this._currentPosition++;
-		if (this._tape.length < this._currentPosition) this._tape.push("");
+		if (this._tape.length < this._currentPosition) this._tape.push("_");
 	},
 	
-	_write: function(symbol) {
+	_write: function(symbol) {				
 		this._tape[this._currentPosition] = symbol;
 	},
 	
@@ -81,7 +100,7 @@ function TransitionTable() {
 
 TransitionTable.prototype = {
 	add: function(state, symbol, newState, newSymbol) {
-		this._table[this._getKey(state, symbol)] = newState + "," + newSymbol;
+		this._table[this._getKey(state, symbol)] = [newState, newSymbol];
 	},
 	
 	get: function(state, symbol) {
